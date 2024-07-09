c0d10621aaac8e6a655ac59451fc791f63ec0545
rajumb0232 <rajugowda0232@gmail.com>
2024-07-09 18:24:55 +0530

    updated the Navigations to load the components lazily, and also added test files for address and contact in backend

--- E-Stores-UI/src/Components/Routes/AllRoutes.jsx
+++ E-Stores-UI/src/Components/Routes/AllRoutes.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import App from "../../App";
 import { navs } from "./Navigations";
 import { useAuth } from "../Auth/AuthProvider";
-const LazyExplore = React.lazy(() => import ("../Public/Explore"))
 
 const AllRoutes = () => {
   const { auth } = useAuth();
@@ -11,32 +10,30 @@ const AllRoutes = () => {
   const [role1, role2] = roles;
 
   return (
-    <Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
         <Route path="/" element={<App />}>
-          {/* {role !== "SELLER" && role !== "ADMIN" && role !== "SUPER_ADMIN" && (
-            <Route path="/explore" element={
-              <React.Suspense fallback="Loading...">
-                <LazyExplore />
-              </React.Suspense>
-            } />
-          )} */}
-
           {navs.map((nav, i) => {
             if (authenticated) {
               if (nav.isVisibleAfterLogin) {
-                if (nav.authorizedTo.includes(role2) ){
-                  return <Route key={i} path={nav.path} element={nav.element} />;
+                if (nav.authorizedTo.includes(role2)) {
+                  return (
+                    <Route key={i} path={nav.path} element={nav.element} />
+                  );
                 }
-                if (nav.authorizedTo.includes(role1) ){
-                  return <Route key={i} path={nav.path} element={nav.element} />;
+                if (nav.authorizedTo.includes(role1)) {
+                  return (
+                    <Route key={i} path={nav.path} element={nav.element} />
+                  );
                 }
               }
-            } else if (!nav.isPrivate){
+            } else if (!nav.isPrivate) {
               return <Route key={i} path={nav.path} element={nav.element} />;
             }
           })}
         </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 };
 
