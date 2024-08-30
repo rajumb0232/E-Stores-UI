3340b0ba39b41a410e538e35b54019e906e9783d
rajumb0232 <rajugowda0232@gmail.com>
2024-08-30 23:26:29 +0530

    updated the allRoutes component to better readability

--- E-Stores-UI/src/Components/Routes/AllRoutes.jsx
+++ E-Stores-UI/src/Components/Routes/AllRoutes.jsx
@@ -14,41 +14,39 @@ import { useAuth } from "../Auth/AuthProvider";
 const AllRoutes = () => {
   const { auth } = useAuth();
   const { authenticated, roles } = auth;
-  const [role1, role2] = roles;
 
   const route = (index, path, element) => (
     <Route key={index} path={path} element={element} />
   );
 
+  const routes = (list) =>
+    list.map((item, index) => (
+      <Route key={index} path={item.path} element={item.element} />
+    ));
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<App />}>
-
           {/* Public Routes */}
-          {noAuthRoutes.map((r, i) => route(i, r.path, r.element))}
+          {route(noAuthRoutes)}
 
           {/* Pre Authentication Routes */}
-          {!authenticated &&
-            preAuthRoutes.map((r, i) => route(i, r.path, r.element))}\
-          
+          {!authenticated && route(preAuthRoutes)}
+
           {/* Common Post Authentication Routes */}
-          {authenticated &&
-            commonPostAuthRoutes.map((r, i) => route(i, r.path, r.element))}
+          {authenticated && routes(commonPostAuthRoutes)}
 
           {/* Seller Routes */}
-          {authenticated &&
-            roles.includes("SELLER") &&
-            sellerRoutes.map((r, i) => route(i, r.path, r.element))}
+          {authenticated && roles.includes("SELLER") && routes(sellerRoutes)}
 
           {/* Customer Routes */}
           {authenticated &&
             roles.includes("CUSTOMER") &&
-            customerRoutes.map((r, i) => route(i, r.path, r.element))}
+            routes(customerRoutes)}
 
           {/* Fallback route */}
           <Route path="*" element={<Navigate to="/" />} />
-          
         </Route>
       </Routes>
     </Suspense>
@@ -56,23 +54,3 @@ const AllRoutes = () => {
 };
 
 export default AllRoutes;
-
-// {
-//   navs.map((nav, i) => {
-//     if (authenticated) {
-//       /* making sure that only routes that should be availabel ofter login is returned */
-//       if (nav.isVisibleAfterLogin) {
-//         /* two if conditions makes sure that routes with specific to that role is only returned */
-//         if (nav.authorizedTo.includes(role2)) {
-//           return <Route key={i} path={nav.path} element={nav.element} />;
-//         }
-//         if (nav.authorizedTo.includes(role1)) {
-//           return <Route key={i} path={nav.path} element={nav.element} />;
-//         }
-//       }
-//       /* if the user is not logged in, the routes that are not private is returned */
-//     } else if (!nav.isPrivate) {
-//       return <Route key={i} path={nav.path} element={nav.element} />;
-//     }
-//   });
-// }
