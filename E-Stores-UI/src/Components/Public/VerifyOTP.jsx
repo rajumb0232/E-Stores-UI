895e6275874f2b3b77c315ed11257483eec4d4fe
rajumb0232 <rajugowda0232@gmail.com>
2024-09-04 16:15:18 +0530

    updated VerifyOTP component, fixed submit issues

--- E-Stores-UI/src/Components/Public/VerifyOTP.jsx
+++ E-Stores-UI/src/Components/Public/VerifyOTP.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../Auth/AuthProvider";
-import AxiosPrivateInstance from "../API/AxiosPrivateInstance";
+import { useAuth } from "../../Auth/AuthProvider";
+import AxiosPrivateInstance from "../../API/AxiosPrivateInstance";
 import { LuShoppingCart } from "react-icons/lu";
 import { RiShoppingBag2Line } from "react-icons/ri";
 import { HiOutlineTag } from "react-icons/hi2";
@@ -30,31 +30,44 @@ const VerifyOTP = () => {
   }, []);
 
   const verifyOTP = async () => {
-    if (otp !== 0) {
-      // requesting to verify OTP
-      try {
-        const response = await axiosInstance.post("/verify-email", {
-          email: email,
-          otp: otp,
-        });
-        if (response.status === 417 || response.status === 400) {
-          console.log(response.data);
-          setIncorrectOTP(response.data.rootCause);
-        }
-        if (response.status === 200) {
-          console.log(response.data);
-          setIncorrectOTP("");
-          sessionStorage.removeItem("userId");
-          navigate("/login");
-        }
-      } catch (error) {
-        console.log(error);
+    console.log("Verifying OTP...");
+    // requesting to verify OTP
+    try {
+      const response = await axiosInstance.post("/verify-email", {
+        email: email,
+        otp: otp,
+      });
+      if (response.status === 417 || response.status === 400) {
+        console.log(response.data);
+        setIncorrectOTP(response.data.rootCause);
       }
+      if (response.status === 200) {
+        console.log(response.data);
+        setIncorrectOTP("");
+        sessionStorage.removeItem("userId");
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("handling Submit");
+    // generating OTP from all the input fields
+    let inputOtp = "";
+    inputs.map((input) => {
+      inputOtp += input.current.value.toString();
+    });
+    setIsSubmited(true);
+    console.log(inputOtp);
+    setOtp(parseInt(inputOtp));
   };
 
   useEffect(() => {
-    verifyOTP();
+    if (otp !== 0) verifyOTP();
   }, [otp]);
 
   const handleChange = (index, event) => {
@@ -72,33 +85,22 @@ const VerifyOTP = () => {
       inputs[index - 1].current.focus();
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // generating OTP from all the input fields
-    let inputOtp = "";
-    inputs.map((input) => {
-      inputOtp += input.current.value.toString();
-    });
-    setIsSubmited(true);
-    setOtp(parseInt(inputOtp));
-  };
 
   return (
-    <div className="w-svw h-svh flex flex-row justify-center items-start bg-slate-100">
-      <div className="w-4/6 h-4/6 mt-24 bg-white flex flex-row justify-center items-center shadow-sm">
-        <div className="w-4/12 bg-slate-600 h-full rounded-l-md flex flex-col justify-center items-center">
-          <div className="p-2 text-white mt-4">
-            <p className="text-4xl font-semibold">Verify Your Email!</p>
-            <p className="text-lg my-6 font-extralight text-white">
+    <div className="w-screen h-screen font-two flex flex-col items-center justify-start">
+      <div className="flex flex-row justify-center items-center w-4/6 h-4/5 mt-24 rounded-md bg-white border border-gray-400 overflow-hidden">
+        <div className="w-full bg-gray-100 h-full rounded-l-md flex flex-col justify-center items-center">
+          <div className="p-6 text-slate-700 mt-4">
+            <p className="text-4xl font-semibold">Verify Your Email 🔑</p>
+            <p className="text-lg my-6 text-slate-700">
               Please Check your mail ID *****{hiddenEmail} for the OPT.
             </p>
           </div>
-          <div className="mt-auto mb-5 flex justify-around text-white p-2 w-3/5 text-2xl">
+          <div className="mt-auto mb-5 flex justify-around text-slate-700 p-2 w-3/5 text-2xl">
             <LuShoppingCart /> <RiShoppingBag2Line /> <HiOutlineTag />
           </div>
         </div>
-        <div className="h-full w-8/12 flex flex-col justify-center items-center mr-auto">
+        <div className="h-full w-full flex flex-col justify-center items-center mr-auto">
           <div className="h-full w-full flex flex-col justify-start items-center my-20">
             <p className="text-lg text-slate-500 py-2">
               Please enter your OTP here
@@ -113,7 +115,7 @@ const VerifyOTP = () => {
                   ref={inputs[i]}
                   onChange={(e) => handleChange(i, e)}
                   onKeyDown={(e) => handleKeyDown(i, e)}
-                  className="bg-cyan-950 bg-opacity-5 focus:border-slate-300 hover:border-slate-300 border-2 border-slate-200  rounded-md m-2 w-12 h-12 text-center text-2xl font-semibold text-slate-700"
+                  className="bg-cyan-950 bg-opacity-5 focus:border-slate-400 hover:border-slate-400 border border-slate-300  rounded-md m-2 w-12 h-12 text-center text-2xl font-semibold text-slate-700"
                 />
               ))}
             </div>
@@ -121,10 +123,10 @@ const VerifyOTP = () => {
               {incorrectOTP}
             </p>
 
-            <div className="w-full flex justify-center items-center">
-              <div className="w-max">
+            <div className="w-full flex justify-center items-center px-8">
+              <div className="w-full">
                 <SubmitBtn
-                  submit={handleSubmit}
+                  onClick={handleSubmit}
                   isSubmited={isSubmited}
                   name={"Confirm"}
                 />
